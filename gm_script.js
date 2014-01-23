eef5acae92ef305ad67bae5909530914ce6b9cd1
Elias Martinez Cohen <eliasmartinezcohen@gmail.com>
2014-01-23 17:09:06 -0500

    initial commit

---
+++ gm_script.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name 			cs032 NiceSite
+// @namespace		http://eliasmartinezcohen.com/
+// @include     	http://cs.brown.edu/courses/cs032/*
+// 
+// Source the new styles from github
+// @resource 	niceStyles	[url to .txt]
+// 
+// ==/UserScript==
+
+/* Delete spongebob shit */
+console.log("removing banner...");
+var _banner = document.getElementById("banner-container");
+_banner.parentNode.removeChild(_banner); //delete pic banner
+console.log("done rm banner");
+
+console.log('start: add CSS');
+var nice_styles  = GM_getResourceText("niceStyles");
+GM_addStyle(nice_styles);
+console.log('done: add CSS');
+
+$(".header-element").hover(function() {
+	this.toggleClass('hovered'); //toggle on over
+}, function() {
+	this.toggleClass('hovered'); //toggle on mouse out
+});
